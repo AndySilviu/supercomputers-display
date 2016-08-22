672300c46e3576ecd1cb2ceb294da8a73edade36
Andrei Mezei <silviuandre@yahoo.com>
2016-08-22 03:57:36 +0100

    Add files via upload

--- getinfo.sh
+++ getinfo.sh
@@ -1,10 +1,12 @@
 #!/usr/bin/bash
 
 upout=$(uptime)
+nvidiaout=$(nvidia-smi | grep Default)
 
-nvidia-smi | grep Default  | awk '{print $9 " " $11 " " $13}' > /sysmon/gpuinfo
-free | grep : > /sysmon/meminfo
-echo "$upout" | awk '{print $10 $11 $12}' > /sysmon/cpuinfo
-df -h | grep ^/dev | awk '{print $2 " " $3 " " $4 " " $5}' > /sysmon/diskinfo
-echo "$upout" | awk '{print $6}' > /sysmon/userinfo
+echo "$nvidiaout" | awk '{print $13}' > /sysmon/gpuinfo.txt
+echo "$nvidiaout" | awk '{print $9 " " $11}' > /sysmon/gpumeminfo.txt
+free | grep : > /sysmon/meminfo.txt
+echo "$upout" | awk '{print $10 $11 $12}' > /sysmon/cpuinfo.txt
+df | grep / | awk '{print $3 " " $4 " "}' > /sysmon/diskinfo.txt
+echo "$upout" | awk '{print $6}' > /sysmon/userinfo.txt
 
